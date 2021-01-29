357ae24db66d06d9669e55fb247db7d48fb90aa1
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-01-29 23:30:51 +0100

    Improve syntax to please Rails 6's loader

--- lib/dradis/plugins/saint/engine.rb
+++ lib/dradis/plugins/saint/engine.rb
@@ -1,13 +1,9 @@
-module Dradis
-  module Plugins
-    module Saint
-      class Engine < ::Rails::Engine
-        isolate_namespace Dradis::Plugins::Saint
+module Dradis::Plugins::Saint
+  class Engine < ::Rails::Engine
+    isolate_namespace Dradis::Plugins::Saint
 
-        include ::Dradis::Plugins::Base
-        description 'Processes SAINT XML format'
-        provides :upload
-      end
-    end
+    include ::Dradis::Plugins::Base
+    description 'Processes SAINT XML format'
+    provides :upload
   end
 end
