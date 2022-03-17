fc69b44e43941d2e2dd0244e9cb376229649f7f4
Brian Pearce <brian.o.pearce@gmail.com>
2022-03-17 15:22:57 +0100

    Register templates locally

--- lib/dradis/plugins/saint/engine.rb
+++ lib/dradis/plugins/saint/engine.rb
@@ -5,5 +5,9 @@ module Dradis::Plugins::Saint
     include ::Dradis::Plugins::Base
     description 'Processes SAINT XML format'
     provides :upload
+
+    def self.template_names
+      { module_parent => { evidence: 'evidence', issue: 'vulnerability' } }
+    end
   end
 end
