0ff7809fa949163579c77c10b0fb58cf812e9c47
Matt Budz <matt.budz@protonmail.com>
2024-03-18 19:36:04 +0100

    use constant for default mappings

--- lib/dradis/plugins/saint/mapping.rb
+++ lib/dradis/plugins/saint/mapping.rb
@@ -1,31 +1,29 @@
 module Dradis::Plugins::Saint
   module Mapping
-    def self.default_mapping
-      {
-        'evidence' => {
-          'Port' => '{{ saint[evidence.port] }}',
-          'Severity' => '{{ saint[evidence.severity] }}',
-          'Class' => '{{ saint[evidence.class] }}',
-          'cve' => '{{ saint[evidence.cve] }}',
-          'CVSS Base Score' => '{{ saint[evidence.cvss_base_score] }}'
-        },
-        'vulnerability' => {
-          'Title' => '{{ saint[vulnerability.description] }}',
-          'Host Name' => '{{ saint[vulnerability.hostname] }}',
-          'IP Address' => '{{ saint[vulnerability.ipaddr] }}',
-          'Host Type' => '{{ saint[vulnerability.hosttype] }}',
-          'Scan Time' => '{{ saint[vulnerability.scan_time] }}',
-          'Status' => '{{ saint[vulnerability.status] }}',
-          'Severity' => '{{ saint[vulnerability.severity] }}',
-          'CVE' => '{{ saint[vulnerability.cve] }}',
-          'CVSS Base Score' => '{{ saint[vulnerability.cvss_base_score] }}',
-          'Impact' => '{{ saint[vulnerability.impact] }}',
-          'Background' => '{{ saint[vulnerability.background] }}',
-          'Problem' => '{{ saint[vulnerability.problem] }}',
-          'Resolution' => '{{ saint[vulnerability.resolution] }}',
-          'Reference' => '{{ saint[vulnerability.reference] }}'
-        }
+    DEFAULT_MAPPING = {
+      evidence: {
+        'Port' => '{{ saint[evidence.port] }}',
+        'Severity' => '{{ saint[evidence.severity] }}',
+        'Class' => '{{ saint[evidence.class] }}',
+        'cve' => '{{ saint[evidence.cve] }}',
+        'CVSS Base Score' => '{{ saint[evidence.cvss_base_score] }}'
+      },
+      vulnerability: {
+        'Title' => '{{ saint[vulnerability.description] }}',
+        'Host Name' => '{{ saint[vulnerability.hostname] }}',
+        'IP Address' => '{{ saint[vulnerability.ipaddr] }}',
+        'Host Type' => '{{ saint[vulnerability.hosttype] }}',
+        'Scan Time' => '{{ saint[vulnerability.scan_time] }}',
+        'Status' => '{{ saint[vulnerability.status] }}',
+        'Severity' => '{{ saint[vulnerability.severity] }}',
+        'CVE' => '{{ saint[vulnerability.cve] }}',
+        'CVSS Base Score' => '{{ saint[vulnerability.cvss_base_score] }}',
+        'Impact' => '{{ saint[vulnerability.impact] }}',
+        'Background' => '{{ saint[vulnerability.background] }}',
+        'Problem' => '{{ saint[vulnerability.problem] }}',
+        'Resolution' => '{{ saint[vulnerability.resolution] }}',
+        'Reference' => '{{ saint[vulnerability.reference] }}'
       }
-    end
+    }.freeze
   end
 end
