eec98b265906540951268bbb03ff41b1ff00ecd6
Matt Budz <matt.budz@protonmail.com>
2024-04-04 18:19:31 +0200

    move fields files to constant

--- lib/dradis/plugins/saint/mapping.rb
+++ lib/dradis/plugins/saint/mapping.rb
@@ -25,5 +25,31 @@ module Dradis::Plugins::Saint
         'Reference' => '{{ saint[vulnerability.reference] }}'
       }
     }.freeze
+
+    SOURCE_FIELDS = {
+      evidence: [
+        'evidence.port',
+        'evidence.severity',
+        'evidence.class',
+        'evidence.cve',
+        'evidence.cvss_base_score'
+      ],
+      vulnerability: [
+        'vulnerability.description',
+        'vulnerability.hostname',
+        'vulnerability.ipaddr',
+        'vulnerability.hosttype',
+        'vulnerability.scan_time',
+        'vulnerability.status',
+        'vulnerability.severity',
+        'vulnerability.cve',
+        'vulnerability.cvss_base_score',
+        'vulnerability.impact',
+        'vulnerability.background',
+        'vulnerability.problem',
+        'vulnerability.resolution',
+        'vulnerability.reference'
+      ]
+    }.freeze
   end
 end
